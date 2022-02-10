cd50072e9a0dd5e6dbfaabd137a6053a1073abd0
AliciaBenavides <77942770+AliciaBenavides@users.noreply.github.com>
2022-02-09 22:24:24 -0500

    update

--- app.js
+++ app.js
@@ -1,30 +1,48 @@
-let cart = [];
-console.log(cart);
+{
+let text1 = "This is my";
+let text2 = "string variable example"; //string variable
+let text3 = text1.concat(" ",text2);
 
-const addToCart = document.getElementById("addToCart");
-addToCart.addEventListener("click", buyNow);
-
-function buyNow(){  //function
-    cart.push("superstar");
-    console.log(cart);
+console.log(text3);
 }
 
-function add(num1, num2) {  //parameter
-    console.log(num1+num2);
+
+let grade = 85
+if (grade > 70){ 
+console.log("Passing score"); //if conditional statement
 }
 
-add(4, 4);
+let day;    //switch statement
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case  6:
+    day = "Saturday"; }
+    console.log(day);
+   
+
+let str = "Web Design 1, Digital Media Programming, Web Design 2"; //string method
+let part = str.slice(14, 39);
+console.log(part);
 
 
-const shoe = {type:"Superstar", year:"2022", color:"white"}; //object with properties
-    console.log(shoe);
-
-
-const person = {  //method
-        firstName: "Alicia",
-        lastName: "Benavides",
-        id: 5566,
-        fullName: function() {
-          return this.firstName + " " + this.lastName;
-        console.log(fullName);}
-}
+let x = 2022;
+console.log(x.toString());
+console.log((2022).toString());
+console.log((2000 + 22).toString());
